ff1c02774239009accbba08dfbc6e2698d637c1a
bongocvidai <115400841+bongocvidai@users.noreply.github.com>
2022-11-16 22:19:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 home='/data/data/com.termux/files/'
-printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\033[0m\n\n'
 termux-setup-storage
 apt update
 apt upgrade
-printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
+printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
 pkg install python
 pip install --upgrade pip
 pip install requests
 curl -s https://ngocbansub.com/ngoctool.py -o $home/usr/bin/ncngoc
 chmod 777 $home/usr/bin/ncngoc
 
-printf '\n\033[1;32m Gõ \033[1;33mncngoc \033[1;32mĐể Vào Tool <3\n\n'
+printf '\n\033[1;32m Gõ \033[1;33mncngoc \033[1;32mĐể Vào Tool \n\n'
