aeff7db50b3208fb8ebe4ba19c731aebb77405e6
Nguyễn Chính Ngọc <115400841+bongocvidai@users.noreply.github.com>
2023-09-16 16:22:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt upgrade
 printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
 pkg install python
 pip install requests
-curl -s https://raw.githubusercontent.com/bongocvidai/RunTool/main/ngocv3.2.py -o $home/usr/bin/ncngoc
+curl -s https://raw.githubusercontent.com/bongocvidai/RunTool/main/ngocv3.3.py -o $home/usr/bin/ncngoc
 chmod 777 $home/usr/bin/ncngoc
 
 printf '\n\033[1;32m Gõ \033[1;33mncngoc \033[1;32mĐể Vào Tool \n\n'
